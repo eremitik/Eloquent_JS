6edf48a0dd964c228b6ed95a5f42320a5d4bd8e0
eremitik <39305876+eremitik@users.noreply.github.com>
2020-11-22 08:41:47 +0900

    Add files via upload

---
+++ eloJS_chpt3_exercises.js
@@ -0,0 +1,27 @@
+//Minimum
+function min(num1, num2){
+	if (num1>num2)
+		return num2;
+	else
+		return num1;
+}
+console.log(min(0, 10));
+
+
+
+//Recursion
+function isEven(num){
+	if (num<0)
+		return "??"
+	else if (num==0)
+		return true;
+	else if (num==1)
+		return false;
+	else
+		return isEven(num-2);
+}
+console.log(isEven(50));
+
+
+
+//Bean Counting (WIP)
