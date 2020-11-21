440d28cb314f8e30af10520675e787b49ad1d05a
eremitik <39305876+eremitik@users.noreply.github.com>
2020-11-22 08:46:33 +0900

    Update eloJS_chpt3_exercises.js

--- eloJS_chpt3_exercises.js
+++ eloJS_chpt3_exercises.js
@@ -1,9 +1,9 @@
 //Minimum
 function min(num1, num2){
-	if (num1>num2)
-		return num2;
-	else
-		return num1;
+  if (num1>num2)
+    return num2;
+  else
+    return num1;
 }
 console.log(min(0, 10));
 
@@ -11,14 +11,14 @@ console.log(min(0, 10));
 
 //Recursion
 function isEven(num){
-	if (num<0)
-		return "??"
-	else if (num==0)
-		return true;
-	else if (num==1)
-		return false;
-	else
-		return isEven(num-2);
+  if (num<0)
+    return "??"
+  else if (num==0)
+    return true;
+  else if (num==1)
+    return false;
+  else
+    return isEven(num-2);
 }
 console.log(isEven(50));
 
