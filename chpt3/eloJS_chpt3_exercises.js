7b3e0bc6ed91e463f9043aab54ce9e19ddde6580
eremitik <39305876+eremitik@users.noreply.github.com>
2020-12-03 18:28:04 +0900

    Update eloJS_chpt3_exercises.js

--- chpt3/eloJS_chpt3_exercises.js
+++ chpt3/eloJS_chpt3_exercises.js
@@ -7,6 +7,19 @@ function min(num1, num2){
 }
 console.log(min(0, 10));
 
+//better version (one week later):
+function min (a,b){
+  return a===b ? a : a>b ? b : a
+}
+
+//better again (two weeks later):
+function min(a,b){
+  return a<b?a:b
+}
+
+
+
+
 
 
 //Recursion
@@ -22,9 +35,17 @@ function isEven(num){
 }
 console.log(isEven(50));
 
+//better version (two weeks later):
+function isEven(n){
+  return n===0?true:n===1?false:n<0?'??':isEven(n-2)
+}
 
 
-//Bean Counting
+
+
+
+
+//Bean Counting (WIP)
 function countBs(word){
   var count = 0
   for (i=0; i<word.length; i++)
@@ -47,3 +68,13 @@ function countChar(word, letter){
   return count
 }
 console.log(countChar('kakkerlak', 'k'))
+
+
+//better version (two weeks later):
+function countChar(str, char){
+  var count = 0
+  for (i=0; i<str.length; i++){
+    str.charAt(i) === char ? count+=1 : count+=0
+  }
+  return count
+}
