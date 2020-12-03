a923c169e4b555e07616b74430de8283313052eb
eremitik <39305876+eremitik@users.noreply.github.com>
2020-12-03 18:27:50 +0900

    Update eloJS_chpt2_exercises.js

--- chpt2/eloJS_chpt2_exercises.js
+++ chpt2/eloJS_chpt2_exercises.js
@@ -5,11 +5,19 @@ for (i=0; i<7; i++)
 
 
 
+
 //FizzBuzz
 for (i=1; i<=100; i++)
-  console.log(i%3==0 && i%5==0 ? 'FizzBuzz':
-                        i%3==0 ? 'Fizz': 
-                        i%5==0 ? 'Buzz': i)
+  console.log(i%3==0 && i%5==0 ? 
+          'FizzBuzz'  : i%3==0 ? 
+          'Fizz'      : i%5==0 ? 
+          'Buzz'      : i)
+
+//better version
+for (i=0; i<=100; i++){
+  var num = 0
+  num += i
+  console.log(i%3===0 && i%5===0 ? 'FizzBuzz' : i%3===0 ? 'Fizz' : i%5===0 ? 'Buzz' : i)
 
 
 
@@ -23,3 +31,13 @@ for (row=0; row<spaces; row++){
   };
   console.log(board);
 };
+
+//better version
+var spaces = 8
+for (i=0; i<spaces; i++){
+  var board = ''
+  for (j=0; j<spaces; j++){
+    (i+j)%2===0 ? board+=' ' : board+='#'
+  }
+  console.log(board)
+}
